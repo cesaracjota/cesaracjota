fed50a52a7899d1b8bc36f9c5f5a445a25e02a47
Cesar A. Acjota Merma <cesar.acjota@tecsup.edu.pe>
2022-06-26 17:51:54 -0500

    complete my web site

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
-export const eslint = {
-  // Warning: This allows production builds to successfully complete even if
-  // your project has ESLint errors.
-  ignoreDuringBuilds: true,
-};
-export function webpack(config, { isServer }) {
-  if (!isServer) {
-    config.resolve.fallback.fs = false;
-    config.resolve.fallback.net = false;
-    config.resolve.fallback.tls = false;
-    config.resolve.fallback.child_process = false;
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.child_process = false;
+    }
+    return config;
   }
-  return config;
-}
+};
