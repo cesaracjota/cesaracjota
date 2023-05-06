d94a2f3e2ec697aea5a1f130786ce5f5c3dfc22d
cesaracjota <cesar.acjota@tecsup.edu.pe>
2023-05-06 00:37:35 -0500

    complete CRUDS users, certificates, messages

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import { Divider } from '@chakra-ui/react'
 import Blog from '../Blog/Blog'
 import Contenido from './Contenido'
 
-const Home = ({ loading }) => {
+const Home = () => {
     return (
         <>
             <Contenido />
